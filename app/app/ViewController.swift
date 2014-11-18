ccf0d993aa0292bbe450ad41143f98f2df7e3f70
Alexandre Rieux <alexr.3165@gmail.com>
2014-11-18 20:55:42 +0100

    re-initialization

---
+++ app/app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by RIEUX Alexandre on 17/11/2014.
+//  Copyright (c) 2014 RIEUX Alexandre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

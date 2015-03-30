b8c9c050d85a363a9ebfc64533ce8da3fbdcbf62
alex <alexr.3165@gmail.com>
2015-03-30 23:14:23 +0200

    Link session manager class to main arduino file

--- arduino/app/app.ino
+++ arduino/app/app.ino
@@ -1,9 +1,18 @@
-void setup()
-{
-	
+#include "SMDataSessionManager.h"
+
+SMDataSessionManager sessionManager = SMDataSessionManager();
+
+void setup() {
+
+	Serial.begin(9600);
+
+	while(!Serial); // Leonardo/Micro should wait for serial init
+
+	Serial.println(F("SensMove let's go for folk"));
+
+	sessionManager.InitializeBluetooth();
 }
 
-void loop()
-{
-	
+void loop() {
+	sessionManager.BleLoopCommunication();
 }

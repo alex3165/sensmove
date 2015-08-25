d197b72e8288ce2ed1b8313f2d3f5563bf012325
jsebastien <jsebastien.pelerin@gmail.com>
2015-08-25 11:26:58 +0200

    Change poc Accelerometer and add FSR sensors into the skecth for doc

--- hardware_development/program/program.ino
+++ hardware_development/program/program.ino
@@ -3,6 +3,15 @@
 #include "SMDataManager.h"
 #include "SMBLEApplication.h"
 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+long lastDisplayTime;
+float currentLightLevelX = 0;
+float currentLightLevelY = 0;
+float currentLightLevelZ = 0;
 
 // Create an instance of dataManager
 int fsrPins[] = {6,0,2,3,5,4,1}; 
@@ -20,6 +29,11 @@ void setup() {
 	while(!Serial); // Leonardo/Micro should wait for serial init
     // Initialize the SMBLEApplication instance
   bleApplication.initializeBluetooth();
+   if(!accel.begin()) {
+    // There was a problem detecting the LSM303 ... check your connections
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    //while(1);
+  }
 }
 
 
@@ -28,7 +42,24 @@ void setup() {
 *
 */
 void loop() {
-    
+  /*
+  * Get accelerometer values
+  */
+  sensors_event_t accelEvent;
+  accel.getEvent(&accelEvent);
+
+  /*
+  * Map accelerometers values to the led in analog output 13
+  */
+  currentLightLevelX = map(accelEvent.acceleration.x, -20, 20, 0, 255);
+    currentLightLevelY = map(accelEvent.acceleration.y, -20, 20, 0, 255);
+  currentLightLevelZ = map(accelEvent.acceleration.z, -20, 20, 0, 255);
+
+  Serial.print(currentLightLevelX); Serial.print(" \n ");
+    Serial.print(currentLightLevelY); Serial.print(" \n ");
+  Serial.print(currentLightLevelZ); Serial.print(" \n ");  
+  
+  
     if(bleApplication.getSessionStarted()){
       // Session has been started
       
